6d90c48b079da5adfa56bc523d9b672c1412ba22
Rizqillah <rizqillah531@gmail.com>
2022-12-15 20:32:19 +0700

    new method

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from scipy.cluster.hierarchy import dendrogram, linkage
 
-x = [2, 3, 2, 2, 1, 3, 4, 6, 5, 4]
-y = [1, 2, 2, 3, 3, 1, 2, 3, 4, 4]
+# Read JSON File
+json = pd.read_json("data.json")
 
-data = list(zip(x, y))
+# gabungin nilai x dan y
+data = list(zip(json['x'], json['y']))
 
-linkage_data = linkage(data, method='complete', metric='euclidean')
+# Input dari Program
+# x = [2, 3, 2, 2, 1, 3, 4, 6, 5, 4]
+# y = [1, 2, 2, 3, 3, 1, 2, 3, 4, 4]
+# data = list(zip(x, y))
+
+# Methode Linkage
+# 1. single
+# 2. complete
+# 3. average
+# 4. weighted
+# 5. centroid
+# 6. median
+# 7. ward
+linkage_data = linkage(data, method='single', metric='euclidean')
 dendrogram(linkage_data)
 
 plt.show()
